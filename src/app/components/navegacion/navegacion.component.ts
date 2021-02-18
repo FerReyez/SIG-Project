c090c30ba2e9d97cbc6ce93f62604e721cd56bea
Lucifer <fer.reyes151719@gmail.com>
2021-02-18 15:34:10 -0600

    d

--- src/app/components/navegacion/navegacion.component.ts
+++ src/app/components/navegacion/navegacion.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-navegacion',
-    templateUrl: './registro.component.html'
+    templateUrl: './navegacion.component.html'
 })
 
 export class NavegacionComponent{
