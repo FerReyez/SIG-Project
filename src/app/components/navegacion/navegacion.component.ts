781249c96df7d377f3469b41b1b264030b220f0b
Lucifer <fer.reyes151719@gmail.com>
2021-02-18 15:40:34 -0600

    d

--- src/app/components/navegacion/navegacion.component.ts
+++ src/app/components/navegacion/navegacion.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { from } from "rxjs";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-navegacion',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class NavegacionComponent{
-
+    
 }
