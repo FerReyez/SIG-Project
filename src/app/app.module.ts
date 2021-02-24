5573ea61cd4601e6efac8504d269c95d1720234e
Lucifer <fer.reyes151719@gmail.com>
2021-02-23 19:18:57 -0600

    FIn xD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RegistroComponent } from './components/registro/registro.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
-import { NoticiaComponent } from './components/noticia/noticia.component';
 import { HomeComponent } from './components/home/home.component';
 import {APP_ROUTING} from './app.router';
-import { HeadderComponent } from './components/template/headder/headder.component';
-import { NavbarComponent } from './components/template/navbar/navbar.component';
-import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent,
     NavegacionComponent,
-    NoticiaComponent,
-    HomeComponent,
-    HeadderComponent,
-    NavbarComponent,
-    FooterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,10 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
-import { RegistroComponent } from './components/registro/registro.component';
 import { HomeComponent } from './components/home/home.component';
 
 const APP_ROUTES: Routes =[
     { path: 'home', component: HomeComponent },
-    { path: 'registro', component: RegistroComponent },
     { path: '**', pathMatch:'full', redirectTo:'home' }
 ]
 
